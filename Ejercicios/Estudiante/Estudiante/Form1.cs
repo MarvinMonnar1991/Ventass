4bcc882789cab549ccb31c81dd4315257f3de3d3
MarvinMonnar1991 <monnarmarvin@gmail.com>
2021-02-04 00:45:54 -0600

    Ejercicios

---
+++ Ejercicios/Estudiante/Estudiante/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estudiante
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombre;
+            string edad;
+
+            nombre = textBox1.Text;
+            edad = textBox2.Text;
+
+            MessageBox.Show("La edad de " + nombre + "es de: " + edad);
+
+        }
+    }
+}
